2fab8cc311ba0f57a6eef2051d0c0c4bcb55cdf3
longnca <dsbuff01@gmail.com>
2024-06-22 10:16:33 -0600

    add classes of Client, DTO, Entity, and Controller

---
+++ src/com/mycompany/bookstore/controller/BookController.java
@@ -0,0 +1,20 @@
+package com.mycompany.bookstore.controller;
+
+import com.mycompany.bookstore.dto.BookDTO;
+import com.mycompany.bookstore.service.BookService;
+import com.mycompany.bookstore.service.impl.BookServiceImpl;
+
+public class BookController {
+
+    private BookService bookService;
+
+    public BookController() {
+        // Dynamic/Runtime polymorphism
+        this.bookService = new BookServiceImpl();
+    }
+
+    public Long add(BookDTO bookDTO) {
+        // Call to the Service layer add() method
+        return this.bookService.add(bookDTO);
+    }
+}
---
+++ src/com/mycompany/bookstore/testing/BookClient.java
@@ -0,0 +1,29 @@
+package com.mycompany.bookstore.testing;
+
+import com.mycompany.bookstore.controller.BookController;
+import com.mycompany.bookstore.dto.BookDTO;
+
+public class BookClient {
+    public static void main(String[] args) {
+
+        BookController controller = new BookController();
+
+        // Create the BookDTO and call the controller add(book) method
+        BookDTO bookDTO = new BookDTO();
+        bookDTO.setBookId(111L);
+        bookDTO.setAuthorEmail("author1@gmail.com");
+        bookDTO.setAuthorName("Author One");
+        bookDTO.setAvailableQty(10);
+        bookDTO.setName("Book One");
+        bookDTO.setDescription("Book One Description");
+        bookDTO.setPricePerQty(24.5);
+
+        // Call controller add() method and hold the return value
+        Long bookId = controller.add(bookDTO);
+        if(bookId != null) {
+            System.out.println("Book was successfully created with Id: " + bookId);
+        } else {
+            System.out.println("Some errors occurred while creating a new book.");
+        }
+    }
+}
