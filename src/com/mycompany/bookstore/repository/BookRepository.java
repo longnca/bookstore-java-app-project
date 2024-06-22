3614a744e0dab38b7f14bc6bc4fcfd29f527b499
longnca <dsbuff01@gmail.com>
2024-06-22 10:15:56 -0600

    add interfaces and classes of Repository and Service

---
+++ src/com/mycompany/bookstore/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.mycompany.bookstore.repository;
+
+import com.mycompany.bookstore.entity.BookEntity;
+
+public interface BookRepository {
+
+    public Long add(BookEntity bookEntity);
+}
